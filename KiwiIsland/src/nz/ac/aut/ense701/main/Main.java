b0f2c74578034165893c9439e4f319cf59a9dd0a
Jack Yang <365921437@qq.com>
2017-04-16 14:18:15 +1200

    Update Main.java

--- KiwiIsland/src/nz/ac/aut/ense701/main/Main.java
+++ KiwiIsland/src/nz/ac/aut/ense701/main/Main.java
@@ -1,8 +1,18 @@
 package nz.ac.aut.ense701.main;
 
+
+import java.awt.Toolkit;
+import java.awt.event.*;
+import java.awt.*;
+
+import java.awt.event.WindowEvent;
 import nz.ac.aut.ense701.gameModel.Game;
 import nz.ac.aut.ense701.gui.KiwiCountUI;
+import nz.ac.aut.ense701.gui.Instruction;
 
+import nz.ac.aut.ense701.gameModel.Game;
+ import nz.ac.aut.ense701.gui.KiwiCountUI;
+import nz.ac.aut.ense701.gameModel.GameEventListener;
 /**
  * Kiwi Count Project
  * 
@@ -11,26 +21,35 @@ import nz.ac.aut.ense701.gui.KiwiCountUI;
  */
 public class Main 
 {
-    /**
-     * Main method of Kiwi Count.
-     * 
-     * @param args the command line arguments
-     */
+   
+   
     public static void main(String[] args) 
     {
+  
         // create the game object
         final Game game = new Game();
+         final Instruction gui = new Instruction(game);
+         
         // create the GUI for the game
-        final KiwiCountUI  gui  = new KiwiCountUI(game);
+        // final KiwiCountUI gui = new KiwiCountUI(game);
         // make the GUI visible
-        java.awt.EventQueue.invokeLater(new Runnable() 
+        
+            java.awt.EventQueue.invokeLater(new Runnable() 
         {
-            @Override
+           
             public void run() 
             {
+               
                 gui.setVisible(true);
+              
             }
-        });
+
+         
+        }
+            );
+     
+    
     }
+
 
 }
