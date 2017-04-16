86fdbd6e721e28a4c6cfb802d790a07f414a216b
MikeYang <394898139@qq.com>
2017-04-16 15:46:49 +1200

    Add files via upload

--- KiwiIsland/src/nz/ac/aut/ense701/gameModel/Weather.java
+++ KiwiIsland/src/nz/ac/aut/ense701/gameModel/Weather.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,27 +14,97 @@ import java.util.Random;
  * @author Administrator
  */
 public class Weather {
-    public String creatWeather(){
+    private static final Weather WEATHER = new Weather();
+    private double difficulty;
+    private String weatherStr = "Sunny";
+    private StepCounter stepCounter = StepCounter.getSingleTone();
+    private Weather(){
+    }
+    public static Weather getSingleTone(){
+        return WEATHER;
+    }
+    public double getDifficulty() {
+		return difficulty;
+	}
+	public String getWeatherStr() {
+		return weatherStr;
+	}
+	public void setWeatherStr(String weatherStr) {
+		this.weatherStr = weatherStr;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(difficulty);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((stepCounter == null) ? 0 : stepCounter.hashCode());
+		result = prime * result + ((weatherStr == null) ? 0 : weatherStr.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Weather other = (Weather) obj;
+		if (Double.doubleToLongBits(difficulty) != Double.doubleToLongBits(other.difficulty))
+			return false;
+		if (stepCounter == null) {
+			if (other.stepCounter != null)
+				return false;
+		} else if (!stepCounter.equals(other.stepCounter))
+			return false;
+		if (weatherStr == null) {
+			if (other.weatherStr != null)
+				return false;
+		} else if (!weatherStr.equals(other.weatherStr))
+			return false;
+		return true;
+	}
+	public void getWeatherChageStr(){
+        if(stepCounter.getStep()%4==0){
+            this.creatWeather();
+            this.setDifficulty();
+        }
+    }
+    
+    public void setDifficulty(){
+        if("Sunny".equals(this.weatherStr)){
+            this.difficulty = 0.0;
+        }else if("Windy".equals(this.weatherStr)){
+            this.difficulty = 1.0;
+        }else if("Snowy".equals(this.weatherStr)){
+            this.difficulty = 2.5;
+        }else if("Stormy".equals(this.weatherStr)){
+            this.difficulty = 3.0;
+        }
+    }
+    
+    public void creatWeather(){
         Random rand = new Random();
         int weatherChange = rand.nextInt(4);
-        String weatherStr = null;
         switch(weatherChange){
             case 0:
-                weatherStr = "Sunny";
+                this.weatherStr = "Sunny";
             break;
             case 1:
-                weatherStr = "Rainy";
+                this.weatherStr = "Rainy";
             break;
             case 2:
-                weatherStr = "Windy";
+                this.weatherStr = "Windy";
             break;
             case 3:
-                weatherStr = "Stormy";
+                this.weatherStr = "Stormy";
             break;
             default:
-                weatherStr = "Unkown";
+                this.weatherStr = "Unkown";
             break;
         }
-        return weatherStr;
+      
     }
 }
