09c7f55cfc4e088e150d3069af7e5582e0eb5f0f
AUT gpq8299 <394898139@qq.com>
2017-05-13 15:54:43 +1200

    Add Timer.java

---
+++ KiwiIsland/src/nz/ac/aut/ense701/gameModel/Timer.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nz.ac.aut.ense701.gameModel;
+
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import nz.ac.aut.ense701.gui.KiwiCountUI;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Timer extends Thread{
+    public KiwiCountUI kiwiCountUI;
+    private long programStart;
+    
+    
+    public Timer(KiwiCountUI kiwiCountUI) {
+        this.kiwiCountUI = kiwiCountUI;
+        this.programStart =  System.currentTimeMillis();
+    }
+   
+    public long getProgramStart() {
+        return programStart;
+    }
+
+    public void setProgramStart(long programStart) {
+        this.programStart = programStart;
+    }
+    
+    private String format(long timeInput){
+        int minute,second,milli;
+        milli = (int)(timeInput % 1000);
+        timeInput  = timeInput / 1000;
+        second = (int)(timeInput%60);
+        timeInput = timeInput/60;
+        minute = (int)(timeInput%60);
+        return String.format("%02dm:%02ds",minute,second);
+        
+    }
+   
+    
+    @Override
+    public void run() {
+        while (true) {  
+            if(this.kiwiCountUI.game.getState()!=GameState.LOST || this.kiwiCountUI.game.getState()!=GameState.WON){
+                this.kiwiCountUI.setTime(format(System.currentTimeMillis()-programStart));
+            }
+            try {
+            sleep(1);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Timer.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(1);
+        }
+       }
+      
+    }
+    
+}
